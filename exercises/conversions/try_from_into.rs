d5ef8d9e6f7d29d0fe19310678ca0fda859666a0
h888866j <h888866j@126.com>
2022-07-10 09:52:45 +0000

    macros, clippy, quiz4, conversions, advanced_errors. Left only the last test uncompeleted

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -21,7 +21,7 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
+
 
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
@@ -36,6 +36,18 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let red = tuple.0;
+        let green = tuple.1;
+        let blue = tuple.2;
+        if red < 0 || red > 255 || green < 0 || green > 255 || blue < 0 || blue > 255 {
+            return Err(IntoColorError::IntConversion)
+        }
+        let red = tuple.0 as u8;
+        let green = tuple.1 as u8;
+        let blue = tuple.2 as u8;
+        // let tuple = tuple.iter().map(|(a,b,c)|(a as u8, b as u8, c as u8)).map_err(|e| return Err(IntoColorError::IntConversion));//没有这些方法
+        Ok(Color{red, green, blue})
+
     }
 }
 
@@ -43,6 +55,8 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        if arr.iter().any(|x| x < &0 || x > &255) {return Err(IntoColorError::IntConversion)}
+        Ok(Color{red:arr[0] as u8, green:arr[1] as u8, blue:arr[2] as u8})
     }
 }
 
@@ -50,6 +64,9 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() !=3 {return Err(IntoColorError::BadLen)}
+        if slice.iter().any(|x| x < &0 || x > &255) {return Err(IntoColorError::IntConversion)}
+        Ok(Color{red:slice[0] as u8, green:slice[1] as u8, blue:slice[2] as u8})
     }
 }
 
